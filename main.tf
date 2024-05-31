80db4706f64ac98b7f557cde5d60889a594e6d08
Jimi M <jimi@Jimis-MacBook-Pro.local>
2024-05-31 08:13:40 -0400

    script config

--- main.tf
+++ main.tf
@@ -35,24 +35,11 @@ data "template_file" "cloud_init" {
   }
 }
 
-data "local_file" "userdata_script" {
-  filename = "${path.module}/scripts/userdata.sh"
-}
-
-data "template_file" "combined_user_data" {
-  template = <<-EOF
-${data.template_file.cloud_init.rendered}
-
-# Bash script starts here
-${data.local_file.userdata_script.content}
-EOF
-}
-
 resource "aws_instance" "ec2_instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
   key_name      = module.ec2_key_pair.key_pair_name
-  user_data     = data.template_file.combined_user_data.rendered
+  user_data     = data.template_file.cloud_init.rendered
 
   tags = {
     Name        = var.instance_name
--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
-set -e
-
-# Any other setup tasks you want to perform
 echo "User-data script running..."
 
 # Example command to update and install a package
 yum update -y
-yum install -y git
+yum install -y git 
+yum install -y docker 
+yum install -y docker-compose
+yum install -y python3.11
+yum install -y java
+yum install -y maven
+# Validate installations
+validate_install() {
+  if ! command -v $1 &> /dev/null
+  then
+    echo "$1 could not be installed"
+    exit 1
+  else
+    echo "$1 installed successfully"
+  fi
+}
+
+validate_install git
+validate_install docker
+validate_install docker-compose
