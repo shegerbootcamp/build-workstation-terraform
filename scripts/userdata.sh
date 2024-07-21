3bd16c881fcfb2135e2f849303a5eeea99098f71
Jimi M <jimi@Jimis-MacBook-Pro.local>
2024-07-21 17:03:32 -0400

    updated user data

--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -1,33 +1,53 @@
 #!/bin/bash
 
-echo "creating user"
+# Function to set up the SSH user
+ssh_user_setup() {
+  # Add the new user
+  useradd -m -s /bin/bash ${USERNAME}
 
-# Variables from Terraform
-USERNAME="${USERNAME}"
-PUBLIC_KEY_CONTENT="${PUBLIC_KEY_CONTENT}"
+  # Add the user to specified groups
+  usermod -aG wheel,adm,maintuser ${USERNAME}
 
-# Print the variables to verify they are correctly substituted
-echo "Username: $USERNAME"
-echo "Public Key Content: $PUBLIC_KEY_CONTENT"
+  # Grant sudo privileges
+  echo "${USERNAME} ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/${USERNAME}
 
-# Create the user with a specified gecos field
-sudo useradd -m -s /bin/bash -c "User D. Two" $USERNAME
+  # Create .ssh directory for the new user
+  mkdir -p /home/${USERNAME}/.ssh
 
-# Grant sudo privileges
-echo "$USERNAME ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$USERNAME
+  # Add the public key to the authorized_keys file
+  echo "${PUBLIC_KEY_CONTENT}" > /home/${USERNAME}/.ssh/authorized_keys
 
-# Add the user to specified groups
-sudo usermod -aG wheel,adm,systemd-journal,maintuser $USERNAME
+  # Set the correct permissions
+  chown -R ${USERNAME}:${USERNAME} /home/${USERNAME}/.ssh
+  chmod 700 /home/${USERNAME}/.ssh
+  chmod 600 /home/${USERNAME}/.ssh/authorized_keys
+}
 
-# Enable password authentication for SSH
-sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
-sudo systemctl restart sshd
+# Function to install and run Watchmaker
+watchmaker_run() {
+  echo "Installing and running watchmaker"
 
-# Create .ssh directory and add the public key
-sudo mkdir -p /home/$USERNAME/.ssh
-echo "$PUBLIC_KEY_CONTENT" | sudo tee /home/$USERNAME/.ssh/authorized_keys
-sudo chown -R $USERNAME:$USERNAME /home/$USERNAME/.ssh
-sudo chmod 700 /home/$USERNAME/.ssh
-sudo chmod 600 /home/$USERNAME/.ssh/authorized_keys
+  PYPI_URL=https://pypi.org/simple
 
-echo "User setup completed successfully!"
+  # Setup terminal support for UTF-8
+  export LC_ALL=en_US.UTF-8
+  export LANG=en_US.UTF-8
+
+  # Install pip
+  python3 -m ensurepip
+
+  # Install setup dependencies
+  python3 -m pip install --index-url="$PYPI_URL" --upgrade pip setuptools
+
+  # Install Watchmaker
+  python3 -m pip install --index-url="$PYPI_URL" --upgrade watchmaker
+
+  # Run Watchmaker
+  watchmaker --log-level debug --log-dir=/var/log/watchmaker
+
+  echo "Watchmaker run completed"
+}
+
+# Execute functions
+ssh_user_setup
+watchmaker_run
